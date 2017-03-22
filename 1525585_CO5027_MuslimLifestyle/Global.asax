394c0d8be0b785a4950c47c9f68f07bfd12ec762
1525585-NurunNazihah <1525585@chester.ac.uk>
2017-03-22 11:12:50 +0800

    Initial Commit

---
+++ 1525585_CO5027_MuslimLifestyle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1525585_CO5027_MuslimLifestyle.Global" Language="C#" %>
---
+++ 1525585_CO5027_MuslimLifestyle/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace _1525585_CO5027_MuslimLifestyle
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
